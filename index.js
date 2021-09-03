e9df0193c23f2d73212a5a2507fb0732ad6c94ff
pranaysankineni <pranayrao475@gmail.com>
2021-09-03 12:12:59 -0400

    add

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ console.log("Here's your header:", header);
 
 
 /***** Deliverable 2 *****/
-
+//const header = document.getElementById('header');
+//console.log("Here's your header:", header);
+header.style.color = 'red';
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+ console.log("PLAYERS array looks like this:", PLAYERS)
+ const player = document.createElement("div.player-container");
 
 
 /***** Deliverable 4 *****/

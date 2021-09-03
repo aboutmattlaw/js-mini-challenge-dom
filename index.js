1a46243d260a7bee46b79304f9402e06698278c5
Matt <aboutmattlaw@gmail.com>
2021-09-03 13:33:15 -0400

    final working page... I think

--- index.js
+++ index.js
@@ -9,9 +9,42 @@ console.log("Here's your header:", header);
 //console.log("Here's your header:", header);
 header.style.color = 'red';
 
+
 /***** Deliverable 3 *****/
- console.log("PLAYERS array looks like this:", PLAYERS)
- const player = document.createElement("div.player-container");
+console.log("PLAYERS array looks like this:", PLAYERS)
+
+
+// grab the HTML area where the players will be added (grab element from DOM)
+ const playersDiv = document.querySelector(".player-container")
+// Iterate through players 
+PLAYERS.forEach(playerObj => {
+// forEach player, I am going to create HTML
+    const div = document.createElement('div')
+    div.className = 'player'
+    div.dataset.number = playerObj.number
+
+
+    const h3 = document.createElement('h3')
+    h3.textContent = playerObj.name
+
+    const img = document.createElement('img')
+    img.src = playerObj.photo
+    img.alt = playerObj.name
+
+    div.append(h3)
+    div.append(img)
+    playersDiv.append(div)
+
+
+
+})
+// ADD that new HTML back into the page!!
+
+
 
 
 /***** Deliverable 4 *****/
+
+const findWrongPlayer = document.querySelector(`[data-number='7']`)
+console.log(findWrongPlayer)
+findWrongPlayer.remove()
